54b06b01431ce6d9beaa10e75eb3c1b9907355e0
Heather Till <heather.till@charter.net>
2019-04-11 16:20:16 -0500

    -lab is complete

--- hello.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = "Jane";
-greeter(user);
--- hello.ts
+++ hello.ts
@@ -1,40 +1,98 @@
 import $ from 'jquery';
 
-$(document).ready(function () {
+let diceArray: Array<Die> = [];  // declare array
 
 
-    class Student {
-        fullName: string;
-        constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-            this.fullName = firstName + " " + middleInitial + " " + lastName;
-        }
+
+class Die {
+    // Field
+    die: HTMLDivElement;  //declared variables
+    value: number;
+
+    constructor() {
+        this.die = document.createElement('div');  // create die div
+        this.die.className = 'randomDie';  //  name div
+        this.value = this.roll();  // assign value
+        this.die.textContent = String(this.value);  //display value
+        document.body.appendChild(this.die);  // add die to document.body
+        this.die.addEventListener('click', () => {
+            this.value = this.roll();  // generates a new value for die clicked
+            this.die.textContent = String(this.value);  // updates display value
+        });
+        this.die.addEventListener('dblclick', () => {
+            this.die.remove();  // removes the die
+            diceArray.splice(diceArray.indexOf(this), 1)  // removes the die from the array
+        })
     }
-    interface Person {
-        firstName: string;
-        lastName: string;
+
+    roll() {
+        return Math.floor(Math.random() * 6) + 1;  // generates a random number when called
     }
-    function greeter(person: Person) {
-        console.log("Hello, " + person.firstName + " " + person.lastName);
+}
+
+// Create die function
+$('#genDie').click(function (): any {
+    diceArray.push(new Die);   //  creates each die and pushes it to the array
+});
+
+//  Reroll all die function
+$('#rollDie').click(() => {
+    for (let i = 0; i < diceArray.length; i++) {
+        diceArray[i].value = diceArray[i].roll();  //  loops through the array of die and generates a new number
+        diceArray[i].die.innerHTML = String(diceArray[i].value);  //  updates the die values in the array
     }
-    let user = new Student("Jane", "M.", "User");
-    greeter(user)
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Sum of all die function
+$('#diceSum').click(() => {
+    let dieSum = 0;
+    diceArray.forEach(randomDie => dieSum += randomDie.value);  //  loops through the array of die and adds their values
+    alert(`The sum the the dice is: ${dieSum}`);  //  alerts the sum
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
